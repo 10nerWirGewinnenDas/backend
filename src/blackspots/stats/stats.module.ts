019455b835e5679599adeb41844a2268213c98c0
Luca Heß <65811753+blockiyt@users.noreply.github.com>
2024-04-06 01:11:09 +0200

    fixed wrong nest gedöns

--- src/blackspots/stats/stats.module.ts
+++ src/blackspots/stats/stats.module.ts
@@ -3,7 +3,6 @@ import { BlackSpotStatsController } from './stats.controller';
 import { BlackSpotStatsService } from './stats.service';
 
 @Module({
-  imports: [BlackSpotStatsController,],
   providers: [BlackSpotStatsService],
   controllers: [BlackSpotStatsController]
 })
